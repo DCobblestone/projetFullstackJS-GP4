ca3779ace52d51763dec4e8626f8d20455173583
Bastien <bastienreturn@outlook.fr>
2020-07-12 14:40:52 +0200

    [Back] Setup appli

--- back/index.js
+++ back/index.js
@@ -16,6 +16,50 @@ MongoClient.connect('mongodb://localhost:27017', function (err, client) {
         var ObjectId = require('mongodb').ObjectId; //récupération de l'objectid
         var collection = db.collection('article');
 
+        app.route('/setup').get(function(req, res, next){
+            var article = {
+                _id: "5f0b043eafec7822948984b6",
+                titre: "Premier article !",
+                contenu: "Lorem ipsum dolor sit amet, consectetuliquam risus lectus, sed efficitur nisl ullamcorper nec. Lorem ipsum dolor sit amet, consectetuliquam risus lectus, sed efficitur nisl ullamcorper nec.",
+                tag: ["Sciences"],
+                datePublication: "11/07/2020",
+                auteur: "Bastien Proudhom"
+            }
+            var categorie1 = {
+                nom: "Sport",
+                idArticles: []
+            }
+            var categorie2 = {
+                nom: "Sciences",
+                idArticles: []
+            }
+            var categorie3 = {
+                nom: "Jeux vidéos",
+                idArticles: []
+            }
+            var categorie4 = {
+                nom: "Politique",
+                idArticles: []
+            }
+            var categorie5 = {
+                nom: "Faits divers",
+                idArticles: ["5f0b043eafec7822948984b6"]
+            }
+            var categorie6 = {
+                nom: "Autres",
+                idArticles: []
+            }
+            //On enregistre dans la table 'url' l'objet url (le créer automatiquement)
+            collection.insert(article);
+            db.collection('categorie').insert(categorie1);
+            db.collection('categorie').insert(categorie2);
+            db.collection('categorie').insert(categorie3);
+            db.collection('categorie').insert(categorie4);
+            db.collection('categorie').insert(categorie5);
+            db.collection('categorie').insert(categorie6);
+
+            res.end('setup ok');
+        })
 
         //liste des points d'entrée
         app.route('/articles')
